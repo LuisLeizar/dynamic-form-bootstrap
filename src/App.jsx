0ee50e366365fb04952a87ff7fa24f36651b62bd
LuisLeizar <luisleizarangel@gmail.com>
2025-03-16 13:39:22 -0600

    feat: Adding Services and CardServices components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Introduction from "./components/Introduction";
 import NavbarMenu from "./components/Navbar/NavbarMenu";
+import Services from "./components/Services";
 
 function App() {
     return (
         <>
             <NavbarMenu />
             <Introduction />
+            <Services />
         </>
     );
 }
